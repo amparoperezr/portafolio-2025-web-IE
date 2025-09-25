f471e1b2ed772711436c227f87f2d17fde6636f4
amparoperezr <amparo.perez.r@mail.pucv.cl>
2025-09-25 10:40:36 -0300

    margenes y link ultima seccion

--- toggle-projects.js
+++ toggle-projects.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const arrowIcon = document.querySelector('.arrow-icon');
 
     toggleButton.addEventListener('click', function() {
-        if (projectsContainer.style.display === 'flex') {
+        if (projectsContainer.style.display === 'block') { // AHORA BUSCA 'block'
             projectsContainer.style.display = 'none';
             arrowIcon.style.transform = 'rotate(0deg)';
         } else {
-            projectsContainer.style.display = 'flex';
+            projectsContainer.style.display = 'block'; // AHORA MUESTRA EL CONTENEDOR CON 'block'
             arrowIcon.style.transform = 'rotate(180deg)';
         }
     });
